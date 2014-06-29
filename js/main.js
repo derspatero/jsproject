7e032171fcdf5d0a2df0ae2e4b0f99a6c56e9915
Edward Lambke <elambke@yahoo.com>
2014-06-29 09:17:39 -0700

    Refactored using jquery

--- js/main.js
+++ js/main.js
@@ -2,23 +2,6 @@
 VARIABLES
 */
 
-//game cell variables
-var cell0 = document.getElementById('cell0');
-var cell1 = document.getElementById('cell1');
-var cell2 = document.getElementById('cell2');
-var cell3 = document.getElementById('cell3');
-var cell4 = document.getElementById('cell4');
-var cell5 = document.getElementById('cell5');
-var cell6 = document.getElementById('cell6');
-var cell7 = document.getElementById('cell7');
-var cell8 = document.getElementById('cell8');
-var cell9 = document.getElementById('cell9');
-var cell10 = document.getElementById('cell10');
-var cell11 = document.getElementById('cell11');
-var cell12 = document.getElementById('cell12');
-var cell13 = document.getElementById('cell13');
-var cell14 = document.getElementById('cell14');
-var cell15 = document.getElementById('cell15');
 
 //get cell variables in array
 var cells = [];
@@ -26,7 +9,7 @@ for(var i=0; i < 16; i++) {
     cells.push(document.getElementById('cell' + i));
 }
 
-//nav variables
+//hud variables
 var startStopGame = document.getElementById('start_end_button');
 var numberOfTurns = document.getElementById('number_of_turns');
 var numberOfMatches = document.getElementById('number_of_matches');
@@ -43,9 +26,7 @@ var cards = [];
 //load images
 for(var i=0; i < 8; i++) {
     var image = new Image();
-    if(i<10) {
-        image.src = 'img/card' + i + '.jpg';
-    }
+    image.src = 'img/card' + i + '.jpg';
     cards.push(image);
 }
 
@@ -61,6 +42,7 @@ for(var i=0; i < 16; i++) {
         cellImages[i] = cards[i-8];
     }
 }
+
 
 /*
 END VARIABLES
@@ -113,90 +95,12 @@ function startStopClick(){
 //in game button functionality
 function gameOn() {
     //turn cards
-    cell0.onclick = turnOverCard0;
-    cell1.onclick = turnOverCard1;
-    cell2.onclick = turnOverCard2;
-    cell3.onclick = turnOverCard3;
-    cell4.onclick = turnOverCard4;
-    cell5.onclick = turnOverCard5;
-    cell6.onclick = turnOverCard6;
-    cell7.onclick = turnOverCard7;
-    cell8.onclick = turnOverCard8;
-    cell9.onclick = turnOverCard9;
-    cell10.onclick = turnOverCard10;
-    cell11.onclick = turnOverCard11;
-    cell12.onclick = turnOverCard12;
-    cell13.onclick = turnOverCard13;
-    cell14.onclick = turnOverCard14;
-    cell15.onclick = turnOverCard15;
+    $('[id^=cell]').click(function (){
+        turnOverCard($(this).attr('id').split('cell')[1]);
+    });
     startStopGame.onclick = startStopClick;
 }
 
-/*
-CELL TURNING FUNCTIONS
-*/
-function turnOverCard0() {
-    turnOverCard(0);
-}
-
-function turnOverCard1() {
-    turnOverCard(1);
-}
-function turnOverCard2() {
-    turnOverCard(2);
-}
-
-function turnOverCard3() {
-    turnOverCard(3);
-}
-
-function turnOverCard4() {
-    turnOverCard(4);
-}
-
-function turnOverCard5() {
-    turnOverCard(5);
-}
-
-function turnOverCard6() {
-    turnOverCard(6);
-}
-
-function turnOverCard7() {
-    turnOverCard(7);
-}
-
-function turnOverCard8() {
-    turnOverCard(8);
-}
-
-function turnOverCard9() {
-    turnOverCard(9);
-}
-
-function turnOverCard10() {
-    turnOverCard(10);
-}
-
-function turnOverCard11() {
-    turnOverCard(11);
-}
-
-function turnOverCard12() {
-    turnOverCard(12);
-}
-
-function turnOverCard13() {
-    turnOverCard(13);
-}
-
-function turnOverCard14() {
-    turnOverCard(14);
-}
-
-function turnOverCard15() {
-    turnOverCard(15);
-}
 
 /*
 END CELL TURNING FUNCTIONS
@@ -205,7 +109,11 @@ END CELL TURNING FUNCTIONS
 //Card turning and match evaluation
 function turnOverCard(i) {
     i = parseFloat(i);
+
+    //if the selected card isn't already displayed and the if a second card hasn't already been selected
     if (cells[i].innerHTML == '' && cardSelected[1] == null) {
+
+        //display selected card face
         cells[i].innerHTML = '<img id="card' + i + '" class="card" src="' + cellImages[i].src + '">';
 
         //if no previous cards are selected this turn
